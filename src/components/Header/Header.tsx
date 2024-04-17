4699bad5763098630fce8e6c8725571bf9503714
MoisesAdame <moisesadame01@gmail.com>
2024-04-17 00:04:28 -0600

    Header component initial set up.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState }  from 'react'
+import { Link, useLocation } from 'react-router-dom';
+import { ROUTES } from '../../routes/constants';
+import './header.css';
+
+const Header: React.FC = () => {
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState<string>('');
+
+  useEffect(() => {
+    setCurrentPath(location.pathname);
+  }, [location.pathname]);
+
+  return (
+    <nav className='header__main-conatiner'>
+      <span className='main-container__title'>
+        <Link to={ROUTES.HOME.path}>Movies DB</Link>
+      </span>
+      <ul className='main-conatiner__list-container'>
+        {Object.values(ROUTES).map(route => (
+          <li key={route.path} className='list-container__element'>
+            <Link to={route.path} className={location.pathname === route.path ? 'list-container__element--active' : ''}>
+              {route.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
