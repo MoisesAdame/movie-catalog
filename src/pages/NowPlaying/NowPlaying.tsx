21441e3e909b324a36d8c0f58d04222fcf85f52c
MoisesAdame <moisesadame01@gmail.com>
2024-04-17 09:11:18 -0600

    Creation of Now Playing page.

---
+++ src/pages/NowPlaying/NowPlaying.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContainer from '../../components/MainContainer/MainContainer';
+
+const NowPlaying = () => {
+  return (
+    <MainContainer title='Now Playing'>
+      <p>
+        Hola Mundo
+      </p>
+    </MainContainer>
+  );
+};
+
+export default NowPlaying;
