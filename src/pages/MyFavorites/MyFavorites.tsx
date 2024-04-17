75ecfbeab977a13e46b35d54861c6ba537f795b6
MoisesAdame <moisesadame01@gmail.com>
2024-04-17 09:11:43 -0600

    Creation of My Favorites page

---
+++ src/pages/MyFavorites/MyFavorites.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContainer from '../../components/MainContainer/MainContainer';
+
+const MyFavorites = () => {
+  return (
+    <MainContainer title='My Favorites'>
+      <p>
+        Hola Mundo
+      </p>
+    </MainContainer>
+  );
+};
+
+export default MyFavorites;
