dbf8b75ce8ff95526e9b0f73925bdbf66297f2cf
MoisesAdame <moisesadame01@gmail.com>
2024-04-17 09:12:06 -0600

    Creation of Top Rated page

---
+++ src/pages/TopRated/TopRated.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContainer from '../../components/MainContainer/MainContainer';
+
+const TopRated = () => {
+  return (
+    <MainContainer title='Top Rated'>
+      <p>
+        Hola Mundo
+      </p>
+    </MainContainer>
+  );
+};
+
+export default TopRated;
