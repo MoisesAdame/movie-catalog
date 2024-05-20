a9e5bdfc672b4642a4e63dcfd45a2fa2c9fb7634
MoisesAdame <moisesadame01@gmail.com>
2024-05-20 12:17:57 -0600

    Refactor App component to use React.FC type

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <>
       <RouterProvider router={router}/>
